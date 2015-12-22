7e2887f7062fc083d75b74ee716d32be607b582a
Michiel Appelman <michiel@appelman.se>
2015-12-22 10:41:23 +0100

    Converted indentation to spaces

--- day1_apartment_test.py
+++ day1_apartment_test.py
@@ -3,31 +3,31 @@ import day1_apartment
 import unittest
 
 class TestMoveUpDown(unittest.TestCase):
-	known_values = ( ('(())', 0),
-					('()()', 0),
-					('(((', 3),
-					('(()(()(', 3),
-					('))(((((', 3),
-					('())', -1),
-					('))(', -1),
-					(')))', -3),
-					(')())())', -3))
-	invalid_values = ( '()a' ,
-						'a(',
-						'&()', 
-						'*kc',
-						'89')
+    known_values = ( ('(())', 0),
+                    ('()()', 0),
+                    ('(((', 3),
+                    ('(()(()(', 3),
+                    ('))(((((', 3),
+                    ('())', -1),
+                    ('))(', -1),
+                    (')))', -3),
+                    (')())())', -3))
+    invalid_values = ( '()a' ,
+                        'a(',
+                        '&()', 
+                        '*kc',
+                        '89')
 
-	def test_valid_move_up_down(self):
-		for instruction, floor in self.known_values:
-			result = day1_apartment.move_up_down(instruction)
-			self.assertEqual(floor, result)
+    def test_valid_move_up_down(self):
+        for instruction, floor in self.known_values:
+            result = day1_apartment.move_up_down(instruction)
+            self.assertEqual(floor, result)
 
-	def test_invalid(self):
-		for instruction in self.invalid_values:
-			self.assertRaises(day1_apartment.NonBracket,
-								day1_apartment.move_up_down,
-								instruction)
+    def test_invalid(self):
+        for instruction in self.invalid_values:
+            self.assertRaises(day1_apartment.NonBracket,
+                                day1_apartment.move_up_down,
+                                instruction)
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
