f5f2bdb96fb53c3b9bf80868143b586c3d036690
Michiel Appelman <michiel@appelman.se>
2016-01-13 23:17:16 +0100

    Another small optimization to the minimum
    Removed the sorting operation by using the 'min' function with the 'len'
    key.

---
+++ day17-eggnog.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import itertools
+
+eggnog = 150
+puzzle_input = """43
+3
+4
+10
+21
+44
+4
+6
+47
+41
+34
+17
+17
+44
+36
+31
+46
+9
+27
+38"""
+containers = [ int(line) for line in puzzle_input.split()]
+
+def combo_list(length:int) -> list:
+    i = 0
+    while i < 2**length:
+        yield [ int(combo) for combo in list(format(i, '0{}b'.format(length))) ]
+        i += 1
+
+def get_valid_combinations(eggnog:int, containers:list) -> list:
+    combinations = []
+    for combo in combo_list(len(containers)):
+        container_combo = list(itertools.compress(containers, combo))
+        if sum(container_combo) == eggnog:
+            combinations.append(container_combo)
+    return combinations
+
+def main():
+    combinations = get_valid_combinations(eggnog, containers)
+    right_combinations = [ c for c in combinations 
+                            if len(c) == len(min(combinations, key=len)) ]
+
+    print("Total: {}, Minimum: {}.".format(len(combinations), 
+                                           len(right_combinations)))
+    # Total: 1638, Minimum: 17.
+
+if __name__ == "__main__":
+    main()
